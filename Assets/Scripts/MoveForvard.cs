59cca0572cea107113b987ec28907e292aca7a9d
nnastya17 <nnastya1703@gmail.com>
2022-11-07 13:47:59 +0200

    first commit

---
+++ Assets/Scripts/MoveForvard.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForvard : MonoBehaviour
+{
+    public float speed = 1f;
+    private float topBound = 30;
+    private float lowerBound = -10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+    }
+}
+
